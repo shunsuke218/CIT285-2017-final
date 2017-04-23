55d9e333c2c357054db6e5507805e2f5dcbb3acb
shunsuke218 <shunsuke218@gmail.com>
2017-04-23 10:47:01 -0400

    Update j.bat
    Updates %path% within this batch job.

--- j.bat
+++ j.bat
@@ -1,4 +1,5 @@
 cls
+PATH=C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\system32\WBEM;c:\Program Files\Microsoft SQL Server\90\Tools\binn\;C:\Program Files\Microsoft SQL Server\80\Tools\Binn\;C:\Program Files\Microsoft SQL Server\90\DTS\Binn\;c:\program files\java\jdk1.8.0_91\bin
 del *.class
 echo "Please Execute this batch with Administrator Priviledge."
 xcopy "lib\*.jar" "c:\program files\java\jdk1.8.0_91\jre\lib\ext" /e && ^
